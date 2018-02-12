ca10213c3941183e37d7f68ec1d7d015935a6b46
lushkant <kkchourasiyakk@outlook.com>
2018-02-12 16:49:54 +0530

    Forest WP Customizer Initialization

--- functions.php
+++ functions.php
@@ -1,11 +1,13 @@
 <?php
 
 /* Set Content Width ---------------------------------------------------*/
-if ( ! isset( $content_width ) ) $content_width = 940;
+if ( ! isset( $content_width ) ) {
+	$content_width = 940;
+}
 
 /* Set Retina Cookie -------------------------------------------------------*/
 global $is_retina;
-(isset($_COOKIE['retina'])) ? $is_retina = true : $is_retina = false;
+( isset( $_COOKIE['retina'] ) ) ? $is_retina = true : $is_retina = false;
 
 /**
 * Setup theme defaults and various features.
@@ -20,12 +22,12 @@ if ( ! function_exists( 'stag_theme_setup' ) ) {
 	function stag_theme_setup() {
 
 		/* Load translation domain ---------------------------------------------*/
-		load_theme_textdomain('stag', get_template_directory().'/languages');
+		load_theme_textdomain( 'stag', get_template_directory() . '/languages' );
 
-		$locale = get_locale();
+		$locale      = get_locale();
 		$locale_file = get_template_directory() . "/languages/$locale.php";
 		if ( is_readable( $locale_file ) ) {
-		  require_once( $locale_file );
+			require_once $locale_file;
 		}
 
 		/* Register Menus ------------------------------------------------------*/
@@ -33,7 +35,6 @@ if ( ! function_exists( 'stag_theme_setup' ) ) {
 
 		// This theme styles the visual editor with editor-style.css to match the theme style.
 		// add_editor_style( 'framework/assets/css/editor-style.css' );
-
 		add_theme_support( 'post-thumbnails' );
 		set_post_thumbnail_size( 770, 99999, true );
 
@@ -63,7 +64,7 @@ if ( ! function_exists( 'stag_theme_setup' ) ) {
 		add_theme_support( 'stag-team' );
 	}
 }
-add_action('after_setup_theme', 'stag_theme_setup');
+add_action( 'after_setup_theme', 'stag_theme_setup' );
 
 
 /**
@@ -72,59 +73,69 @@ add_action('after_setup_theme', 'stag_theme_setup');
 * @return void
 */
 if ( ! function_exists( 'stag_sidebar_init' ) ) {
-  function stag_sidebar_init() {
+	function stag_sidebar_init() {
 
-	register_sidebar(array(
-		'name'          => __( 'Global Sidebar', 'stag' ),
-		'id'            => 'sidebar-main',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h3 class="widget-title">',
-		'after_title'   => '</h3>',
-		'description'   => __( 'Blog Widgets Area.', 'stag' ),
-	));
+		register_sidebar(
+			array(
+				'name'          => __( 'Global Sidebar', 'stag' ),
+				'id'            => 'sidebar-main',
+				'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+				'after_widget'  => '</aside>',
+				'before_title'  => '<h3 class="widget-title">',
+				'after_title'   => '</h3>',
+				'description'   => __( 'Blog Widgets Area.', 'stag' ),
+			)
+		);
 
-	register_sidebar(array(
-		'name'          => __( 'Homepage Sections', 'stag' ),
-		'id'            => 'sidebar-homepage',
-		'before_widget' => '<section id="%1$s" class="%2$s"><div class="inside">',
-		'after_widget'  => '</div></section>',
-		'before_title'  => '<h2 class="section-title">',
-		'after_title'   => '</h2>',
-		'description'   => __( 'Here you can configure the layout of the Homepage.', 'stag' ),
-	));
+		register_sidebar(
+			array(
+				'name'          => __( 'Homepage Sections', 'stag' ),
+				'id'            => 'sidebar-homepage',
+				'before_widget' => '<section id="%1$s" class="%2$s"><div class="inside">',
+				'after_widget'  => '</div></section>',
+				'before_title'  => '<h2 class="section-title">',
+				'after_title'   => '</h2>',
+				'description'   => __( 'Here you can configure the layout of the Homepage.', 'stag' ),
+			)
+		);
 
-	register_sidebar(array(
-		'name'          => __( 'About Sections', 'stag' ),
-		'id'            => 'sidebar-about',
-		'before_widget' => '<section id="%1$s" class="%2$s"><div class="inside">',
-		'after_widget'  => '</div></section>',
-		'before_title'  => '<h2 class="section-title">',
-		'after_title'   => '</h2>',
-		'description'   => __( 'About template sections.', 'stag' ),
-	));
+		register_sidebar(
+			array(
+				'name'          => __( 'About Sections', 'stag' ),
+				'id'            => 'sidebar-about',
+				'before_widget' => '<section id="%1$s" class="%2$s"><div class="inside">',
+				'after_widget'  => '</div></section>',
+				'before_title'  => '<h2 class="section-title">',
+				'after_title'   => '</h2>',
+				'description'   => __( 'About template sections.', 'stag' ),
+			)
+		);
 
-	register_sidebar(array(
-		'name'          => __( 'Service Boxes Section', 'stag' ),
-		'id'            => 'sidebar-services',
-		'before_widget' => '<div id="%1$s" class="grid-3 %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h4 class="service-title">',
-		'after_title'   => '</h4>',
-		'description'   => __( 'Use only "Service Box" widgets here and they will populate the "Services" widget.', 'stag' ),
-	));
+		register_sidebar(
+			array(
+				'name'          => __( 'Service Boxes Section', 'stag' ),
+				'id'            => 'sidebar-services',
+				'before_widget' => '<div id="%1$s" class="grid-3 %2$s">',
+				'after_widget'  => '</div>',
+				'before_title'  => '<h4 class="service-title">',
+				'after_title'   => '</h4>',
+				'description'   => __( 'Use only "Service Box" widgets here and they will populate the "Services" widget.', 'stag' ),
+			)
+		);
 
-	register_sidebar(array(
-		'name'          => __( 'Footer Widgets', 'stag' ),
-		'id'            => 'sidebar-footer',
-		'before_widget' => '<div class="grid-3 widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h3 class="widget-title">',
-		'after_title'   => '</h3>',
-		'description'   => __( 'Widget area for footer', 'stag' ),
-	));
+		register_sidebar(
+			array(
+				'name'          => __( 'Footer Widgets', 'stag' ),
+				'id'            => 'sidebar-footer',
+				'before_widget' => '<div class="grid-3 widget %2$s">',
+				'after_widget'  => '</div>',
+				'before_title'  => '<h3 class="widget-title">',
+				'after_title'   => '</h3>',
+				'description'   => __( 'Widget area for footer', 'stag' ),
+			)
+		);
 
-  }
+	}
 }
 add_action( 'widgets_init', 'stag_sidebar_init' );
 
@@ -143,7 +154,7 @@ if ( ! function_exists( 'stag_wp_title' ) ) {
 			$title .= get_bloginfo( 'name' );
 
 			$site_desc = get_bloginfo( 'description', 'display' );
-			if( $site_desc && ( is_home() || is_front_page() ) ) {
+			if ( $site_desc && ( is_home() || is_front_page() ) ) {
 				$title = "$title $sep $site_desc";
 			}
 		}
@@ -151,7 +162,6 @@ if ( ! function_exists( 'stag_wp_title' ) ) {
 	}
 }
 // add_filter('wp_title', 'stag_wp_title', 10, 2);
-
 /**
  * Enqueues scripts and styles for front end.
  *
@@ -177,32 +187,36 @@ function stag_scripts_styles() {
 		}
 
 		if ( is_front_page() || is_page_template( 'template-about.php' ) || is_page_template( 'template-widgetized.php' ) ) {
-		  wp_enqueue_script( 'cycle2' );
+			wp_enqueue_script( 'cycle2' );
 		}
 
 		/* Enqueue Styles ---------------------------------------------------*/
 		wp_dequeue_style( 'stag-shortcode-styles' );
-		wp_enqueue_style( 'shortcode-styles', get_template_directory_uri().'/assets/css/shortcodes.css', '', STAG_THEME_VERSION );
+		wp_enqueue_style( 'shortcode-styles', get_template_directory_uri() . '/assets/css/shortcodes.css', '', STAG_THEME_VERSION );
 
 		wp_enqueue_style( 'stag-style', get_stylesheet_uri(), '', STAG_THEME_VERSION );
-		wp_enqueue_style( 'stag-custom-style', get_template_directory_uri().'/assets/css/stag-custom-styles.php', 'stag-style', STAG_THEME_VERSION );
+		wp_enqueue_style( 'stag-custom-style', get_template_directory_uri() . '/assets/css/stag-custom-styles.php', 'stag-style', STAG_THEME_VERSION );
 		wp_enqueue_style( 'forest-fonts', stag_google_font_url(), array(), null );
 
 		if ( is_page() && post_type_exists( 'portfolio' ) || is_page_template( 'template-homepage.php' ) ) {
-			wp_enqueue_script('mixitup');
+			wp_enqueue_script( 'mixitup' );
 		}
 
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'stag-custom' );
 		wp_enqueue_script( 'stag-plugins' );
 
-		if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); // Loads the javascript required for threaded comments.
+		if ( is_singular() ) {
+			wp_enqueue_script( 'comment-reply' ); // Loads the javascript required for threaded comments.
+		}
 
-		wp_localize_script('stag-custom', 'stag', array(
-			'ajaxurl'  => admin_url( 'admin-ajax.php' ),
-			'accent'   => stag_get_option( 'style_accent_color' ),
-			'menuIcon' => ( function_exists( 'stag_icon' ) ) ? stag_icon( array( 'icon' => 'bars' ) ) : '',
-		));
+		wp_localize_script(
+			'stag-custom', 'stag', array(
+				'ajaxurl'  => admin_url( 'admin-ajax.php' ),
+				'accent'   => stag_get_option( 'style_accent_color' ),
+				'menuIcon' => ( function_exists( 'stag_icon' ) ) ? stag_icon( array( 'icon' => 'bars' ) ) : '',
+			)
+		);
 
 	}
 }
@@ -210,8 +224,8 @@ add_action( 'wp_enqueue_scripts', 'stag_scripts_styles' );
 
 
 /**
-* Comment Styling
-*/
+ * Comment Styling
+ */
 function stag_comments( $comment, $args, $depth ) {
 
 	$isByAuthor = false;
@@ -221,31 +235,40 @@ function stag_comments( $comment, $args, $depth ) {
 	}
 
 	$GLOBALS['comment'] = $comment; ?>
-   <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
+   <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
 	 <div id="comment-<?php comment_ID(); ?>" class="the-comment">
 
 	 <div class="comment-body clearfix">
 		<div class="avatar-wrap">
-		  <?php
+			<?php
 			global $is_retina;
-			if($is_retina){
-			  echo get_avatar($comment, $size='140');
-			}else{
-			  echo get_avatar($comment, $size='70');
+			if ( $is_retina ) {
+				echo get_avatar( $comment, $size = '140' );
+			} else {
+				echo get_avatar( $comment, $size = '70' );
 			}
-		  ?>
+			?>
 		</div>
 		<div class="comment-area">
 			<div class="row">
 			  <span class="comment-author"><?php echo get_comment_author_link(); ?></span>
 			  <span class="comment-date"><?php printf( __( '%s ago', 'stag' ), human_time_diff( get_comment_date( 'U' ), current_time( 'timestamp' ) ) ); ?></span>
-			  <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+				<?php
+				comment_reply_link(
+					array_merge(
+						$args, array(
+							'depth'     => $depth,
+							'max_depth' => $args['max_depth'],
+						)
+					)
+				);
+				?>
 			</div>
-			<?php if ($comment->comment_approved == '0') : ?>
-			   <em class="moderation"><?php _e('Your comment is awaiting moderation.', 'stag') ?></em>
+			<?php if ( $comment->comment_approved == '0' ) : ?>
+			   <em class="moderation"><?php _e( 'Your comment is awaiting moderation.', 'stag' ); ?></em>
 			<?php endif; ?>
 			<div class="comment-text">
-			  <?php comment_text() ?>
+				<?php comment_text(); ?>
 			</div>
 		</div>
 	  </div>
@@ -256,125 +279,136 @@ function stag_comments( $comment, $args, $depth ) {
 <?php
 }
 
-function stag_list_pings($comment, $args, $depth) {
-   $GLOBALS['comment'] = $comment; ?>
+function stag_list_pings( $comment, $args, $depth ) {
+	$GLOBALS['comment'] = $comment;
+	?>
 <li id="comment-<?php comment_ID(); ?>"><?php comment_author_link(); ?>
-<?php }
+<?php
+}
 
 
 /**
 * Custom Excerpt
 */
-if(!function_exists('custom_excerpt_length')){
+if ( ! function_exists( 'custom_excerpt_length' ) ) {
 	function custom_excerpt_length( $length ) {
-		return stag_get_option('general_post_excerpt_length');
+		return stag_get_option( 'general_post_excerpt_length' );
 	}
 	add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 }
 
 
-if(!function_exists('new_excerpt_more')){
-	function new_excerpt_more($more) {
+if ( ! function_exists( 'new_excerpt_more' ) ) {
+	function new_excerpt_more( $more ) {
 		global $post;
-		return ' <a class="read-more" href="'. get_permalink($post->ID) . '">'.stag_get_option('general_post_excerpt_text').'</a>';
+		return ' <a class="read-more" href="' . get_permalink( $post->ID ) . '">' . stag_get_option( 'general_post_excerpt_text' ) . '</a>';
 	}
-	add_filter('excerpt_more', 'new_excerpt_more');
+	add_filter( 'excerpt_more', 'new_excerpt_more' );
 }
 
 /**
 * Allow shortcodes in text widgets
 */
-add_filter('widget_text', 'shortcode_unautop');
-add_filter('widget_text', 'do_shortcode');
+add_filter( 'widget_text', 'shortcode_unautop' );
+add_filter( 'widget_text', 'do_shortcode' );
 
 
 /**
-* Relative Date for comments
-*/
+ * Relative Date for comments
+ */
 // add_filter( 'get_comment_date', 'get_the_relative_time' );
-function get_the_relative_time($time = null){
-	if(is_null($time)) $time = get_the_time("U");
+function get_the_relative_time( $time = null ) {
+	if ( is_null( $time ) ) {
+		$time = get_the_time( 'U' );
+	}
 
-	$time_diff = date("U") - $time; // difference in second
+	$time_diff = date( 'U' ) - $time; // difference in second
 
 	$second = 1;
 	$minute = 60;
-	$hour   = 60*60;
-	$day    = 60*60*24;
-	$week   = 60*60*24*7;
-	$month  = 60*60*24*7*30;
-	$year   = 60*60*24*7*30*365;
+	$hour   = 60 * 60;
+	$day    = 60 * 60 * 24;
+	$week   = 60 * 60 * 24 * 7;
+	$month  = 60 * 60 * 24 * 7 * 30;
+	$year   = 60 * 60 * 24 * 7 * 30 * 365;
 
-	if ($time_diff <= 120) {
-		$output = "now";
-	} elseif ($time_diff > $second && $time_diff < $minute) {
-		$output = round($time_diff/$second)." second";
-	} elseif ($time_diff >= $minute && $time_diff < $hour) {
-		$output = round($time_diff/$minute)." minute";
-	} elseif ($time_diff >= $hour && $time_diff < $day) {
-		$output = round($time_diff/$hour)." hour";
-	} elseif ($time_diff >= $day && $time_diff < $week) {
-		$output = round($time_diff/$day)." day";
-	} elseif ($time_diff >= $week && $time_diff < $month) {
-		$output = round($time_diff/$week)." week";
-	} elseif ($time_diff >= $month && $time_diff < $year) {
-		$output = round($time_diff/$month)." month";
-	} elseif ($time_diff >= $year && $time_diff < $year*10) {
-		$output = round($time_diff/$year)." year";
-	} else{ $output = " more than a decade ago"; }
+	if ( $time_diff <= 120 ) {
+		$output = 'now';
+	} elseif ( $time_diff > $second && $time_diff < $minute ) {
+		$output = round( $time_diff / $second ) . ' second';
+	} elseif ( $time_diff >= $minute && $time_diff < $hour ) {
+		$output = round( $time_diff / $minute ) . ' minute';
+	} elseif ( $time_diff >= $hour && $time_diff < $day ) {
+		$output = round( $time_diff / $hour ) . ' hour';
+	} elseif ( $time_diff >= $day && $time_diff < $week ) {
+		$output = round( $time_diff / $day ) . ' day';
+	} elseif ( $time_diff >= $week && $time_diff < $month ) {
+		$output = round( $time_diff / $week ) . ' week';
+	} elseif ( $time_diff >= $month && $time_diff < $year ) {
+		$output = round( $time_diff / $month ) . ' month';
+	} elseif ( $time_diff >= $year && $time_diff < $year * 10 ) {
+		$output = round( $time_diff / $year ) . ' year';
+	} else {
+		$output = ' more than a decade ago'; }
 
-	if ($output <> "now") {
-		$output = (substr($output,0,2)<>"1 ") ? $output."s" : $output;
-		$output .= " ago";
+	if ( $output <> 'now' ) {
+		$output  = ( substr( $output, 0, 2 ) <> '1 ' ) ? $output . 's' : $output;
+		$output .= ' ago';
 	}
 
-  return $output;
+	return $output;
 }
 
 
-function pagination(){
-  global $wp_query;
+function pagination() {
+	global $wp_query;
 	$total_pages = $wp_query->max_num_pages;
-	if($total_pages > 1){
-	  if ($total_pages > 1){
-		$current_page = max(1, get_query_var('paged'));
-		$return = paginate_links(array(
-			'base' => get_pagenum_link(1) . '%_%',
-			'format' => '/page/%#%',
-			'current' => $current_page,
-			'total' => $total_pages,
-			'prev_next' => false
-		  ));
-		echo "<div class='pages'>{$return}</div>";
+	if ( $total_pages > 1 ) {
+		if ( $total_pages > 1 ) {
+			$current_page = max( 1, get_query_var( 'paged' ) );
+			$return       = paginate_links(
+				array(
+					'base'      => get_pagenum_link( 1 ) . '%_%',
+					'format'    => '/page/%#%',
+					'current'   => $current_page,
+					'total'     => $total_pages,
+					'prev_next' => false,
+				)
+			);
+			echo "<div class='pages'>{$return}</div>";
 		}
-  }else{
-	return false;
-  }
+	} else {
+		return false;
+	}
 }
 
 
 function stag_paging_nav() {
-  global $wp_query;
+	global $wp_query;
 
-  // Don't print empty markup if there's only one page.
-  if ( $wp_query->max_num_pages < 2)
-	return;
-  ?>
+	// Don't print empty markup if there's only one page.
+	if ( $wp_query->max_num_pages < 2 ) {
+		return;
+	}
+	?>
   <nav class="navigation paging-navigation" role="navigation">
-	<?php if( !is_search() ) pagination(); ?>
+	<?php
+	if ( ! is_search() ) {
+		pagination();}
+?>
 	<div class="nav-links">
 
-	  <?php if ( get_previous_posts_link() ) : ?>
+		<?php if ( get_previous_posts_link() ) : ?>
 	  <div class="nav-previous"><?php previous_posts_link( __( '<i class="icon icon-previous"></i>', 'stag' ) ); ?></div>
-	  <?php endif; ?>
+		<?php endif; ?>
 
-	  <?php if ( get_next_posts_link() ) : ?>
+		<?php if ( get_next_posts_link() ) : ?>
 	  <div class="nav-next"><?php next_posts_link( __( '<i class="icon icon-next"></i>', 'stag' ) ); ?></div>
-	  <?php endif; ?>
+		<?php endif; ?>
 
 	</div><!-- .nav-links -->
   </nav><!-- .navigation -->
-  <?php
+	<?php
 }
 
 
@@ -391,17 +425,17 @@ function stag_paging_nav() {
  * TGM_Plugin_Activation class constructor.
  */
 function stag_required_plugins() {
-  $plugins = array(
+	$plugins = array(
 
-	array(
-	  'name'     => 'StagTools',
-	  'slug'     => 'stagtools',
-	  'required' => true,
-	),
+		array(
+			'name'     => 'StagTools',
+			'slug'     => 'stagtools',
+			'required' => true,
+		),
 
-  );
+	);
 
-  tgmpa( $plugins );
+	tgmpa( $plugins );
 }
 add_action( 'tgmpa_register', 'stag_required_plugins' );
 
@@ -411,10 +445,16 @@ add_action( 'tgmpa_register', 'stag_required_plugins' );
  * @since 1.1
  */
 function stag_check_third_party_seo() {
-	include_once(ABSPATH .'wp-admin/includes/plugin.php');
-	if( is_plugin_active('headspace2/headspace.php') ) return true;
-	if( is_plugin_active('all-in-one-seo-pack/all_in_one_seo_pack.php') ) return true;
-	if( is_plugin_active('wordpres-seo/wp-seo.php') ) return true;
+	include_once ABSPATH . 'wp-admin/includes/plugin.php';
+	if ( is_plugin_active( 'headspace2/headspace.php' ) ) {
+		return true;
+	}
+	if ( is_plugin_active( 'all-in-one-seo-pack/all_in_one_seo_pack.php' ) ) {
+		return true;
+	}
+	if ( is_plugin_active( 'wordpres-seo/wp-seo.php' ) ) {
+		return true;
+	}
 	return false;
 }
 
@@ -424,8 +464,8 @@ function stag_check_third_party_seo() {
  * @since  1.1.2
  */
 function stag_date( $arg, $data = null ) {
-  $return = date($arg, strtotime($data));
-  return $return;
+	$return = date( $arg, strtotime( $data ) );
+	return $return;
 }
 
 /**
@@ -439,7 +479,7 @@ function stag_custom_sidebar_widget_wrapper() {
 		'before_widget' => '<section id="%1$s" class="%2$s"><div class="inside">',
 		'after_widget'  => '</div></section>',
 		'before_title'  => '<h2 class="section-title">',
-		'after_title'   => '</h2>'
+		'after_title'   => '</h2>',
 	);
 }
 add_filter( 'stag_custom_sidebars_widget_args', 'stag_custom_sidebar_widget_wrapper' );
@@ -448,5 +488,6 @@ add_filter( 'stag_custom_sidebars_widget_args', 'stag_custom_sidebar_widget_wrap
  * Include framework and other files
  */
 
-include_once ( get_template_directory() . '/framework/stag-framework.php' );
-include_once ( get_template_directory() . '/includes/_init.php' );
+require_once get_template_directory() . '/framework/stag-framework.php';
+require_once get_template_directory() . '/includes/_init.php';
+require_once get_template_directory() . '/includes/theme-customizer.php';
