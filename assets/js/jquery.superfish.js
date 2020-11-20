21de03fd0231a4d921a460089b34e51a940397a9
Krishna Kant <lushkant@gmail.com>
2020-11-20 09:30:58 +0530

    Update Superfish jquery plugin

--- assets/js/jquery.superfish.js
+++ assets/js/jquery.superfish.js
@@ -1,4 +1,370 @@
-/* SUPERFISH */
-(function(b){b.fn.superfish=function(k){var g=b.fn.superfish,j=g.c,f=b(['<span class="',j.arrowClass,'"> &#187;</span>'].join("")),i=function(){var c=b(this),l=d(c);clearTimeout(l.sfTimer);c.showSuperfishUl().siblings().hideSuperfishUl()},e=function(){var c=b(this),m=d(c),l=g.op;clearTimeout(m.sfTimer);m.sfTimer=setTimeout(function(){l.retainPath=(b.inArray(c[0],l.$path)>-1);c.hideSuperfishUl();if(l.$path.length&&c.parents(["li.",l.hoverClass].join("")).length<1){i.call(l.$path)}},l.delay)},d=function(c){var l=c.parents(["ul.",j.menuClass,":first"].join(""))[0];g.op=g.o[l.serial];return l},h=function(c){c.addClass(j.anchorClass).append(f.clone())};return this.each(function(){var c=this.serial=g.o.length;var m=b.extend({},g.defaults,k);m.$path=b("li."+m.pathClass,this).slice(0,m.pathLevels).each(function(){b(this).addClass([m.hoverClass,j.bcClass].join(" ")).filter("li:has(ul)").removeClass(m.pathClass)});g.o[c]=g.op=m;b("li:has(ul)",this)[(b.fn.hoverIntent&&!m.disableHI)?"hoverIntent":"hover"](i,e).each(function(){if(m.autoArrows){h(b(">a:first-child",this))}}).not("."+j.bcClass).hideSuperfishUl();var l=b("a",this);l.each(function(n){var o=l.eq(n).parents("li");l.eq(n).focus(function(){i.call(o)}).blur(function(){e.call(o)})});m.onInit.call(this)}).each(function(){var c=[j.menuClass];if(g.op.dropShadows&&!(b.browser.msie&&b.browser.version<7)){c.push(j.shadowClass)}b(this).addClass(c.join(" "))})};var a=b.fn.superfish;a.o=[];a.op={};a.IE7fix=function(){var c=a.op;if(b.browser.msie&&b.browser.version>6&&c.dropShadows&&c.animation.opacity!=undefined){this.toggleClass(a.c.shadowClass+"-off")}};a.c={bcClass:"sf-breadcrumb",menuClass:"sf-js-enabled",anchorClass:"sf-with-ul",arrowClass:"sf-sub-indicator",shadowClass:"sf-shadow"};a.defaults={hoverClass:"sfHover",pathClass:"overideThisToUse",pathLevels:1,delay:800,animation:{opacity:"show"},speed:"normal",autoArrows:true,dropShadows:true,disableHI:false,onInit:function(){},onBeforeShow:function(){},onShow:function(){},onHide:function(){}};b.fn.extend({hideSuperfishUl:function(){var e=a.op,d=(e.retainPath===true)?e.$path:"";e.retainPath=false;var c=b(["li.",e.hoverClass].join(""),this).add(this).not(d).removeClass(e.hoverClass).find(">ul").hide().css("visibility","hidden");e.onHide.call(c);return this},showSuperfishUl:function(){var e=a.op,d=a.c.shadowClass+"-off",c=this.addClass(e.hoverClass).find(">ul:hidden").css("visibility","visible");a.IE7fix.call(c);e.onBeforeShow.call(c);c.animate(e.animation,e.speed,function(){a.IE7fix.call(c);e.onShow.call(c)});return this}})})(jQuery);
-/* SUPERSUBS */
-(function(a){a.fn.supersubs=function(b){var c=a.extend({},a.fn.supersubs.defaults,b);return this.each(function(){var d=a(this);var e=a.meta?a.extend({},c,d.data()):c;var f=a('<li id="menu-fontsize">&#8212;</li>').css({padding:0,position:"absolute",top:"-999em",width:"auto"}).appendTo(d).width();a("#menu-fontsize").remove();$ULs=d.find("ul");$ULs.each(function(l){var k=$ULs.eq(l);var j=k.children();var g=j.children("a");var m=j.css("white-space","nowrap").css("float");var h=k.add(j).add(g).css({"float":"none",width:"auto"}).end().end()[0].clientWidth/f;h+=e.extraWidth;if(h>e.maxWidth){h=e.maxWidth}else{if(h<e.minWidth){h=e.minWidth}}h+="em";k.css("width",h);j.css({"float":m,width:"100%","white-space":"normal"}).each(function(){var n=a(">ul",this);var i=n.css("left")!==undefined?"left":"right";n.css(i,h)})})})};a.fn.supersubs.defaults={minWidth:9,maxWidth:25,extraWidth:0}})(jQuery);
+/*
+ * jQuery Superfish Menu Plugin - v1.7.10
+ * Copyright (c) 2018 Joel Birch
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *	http://www.opensource.org/licenses/mit-license.php
+ *	http://www.gnu.org/licenses/gpl.html
+ */
+
+;(function ($, w) {
+    "use strict";
+
+    var methods = (function () {
+        // private properties and methods go here
+        var c = {
+                bcClass: 'sf-breadcrumb',
+                menuClass: 'sf-js-enabled',
+                anchorClass: 'sf-with-ul',
+                menuArrowClass: 'sf-arrows'
+            },
+            ios = (function () {
+                var ios = /^(?![\w\W]*Windows Phone)[\w\W]*(iPhone|iPad|iPod)/i.test(navigator.userAgent);
+                if (ios) {
+                    // tap anywhere on iOS to unfocus a submenu
+                    $('html').css('cursor', 'pointer').on('click', $.noop);
+                }
+                return ios;
+            })(),
+            wp7 = (function () {
+                var style = document.documentElement.style;
+                return ('behavior' in style && 'fill' in style && /iemobile/i.test(navigator.userAgent));
+            })(),
+            unprefixedPointerEvents = (function () {
+                return (!!w.PointerEvent);
+            })(),
+            toggleMenuClasses = function ($menu, o, add) {
+                var classes = c.menuClass,
+                    method;
+                if (o.cssArrows) {
+                    classes += ' ' + c.menuArrowClass;
+                }
+                method = (add) ? 'addClass' : 'removeClass';
+                $menu[method](classes);
+            },
+            setPathToCurrent = function ($menu, o) {
+                return $menu.find('li.' + o.pathClass).slice(0, o.pathLevels)
+                    .addClass(o.hoverClass + ' ' + c.bcClass)
+                    .filter(function () {
+                        return ($(this).children(o.popUpSelector).hide().show().length);
+                    }).removeClass(o.pathClass);
+            },
+            toggleAnchorClass = function ($li, add) {
+                var method = (add) ? 'addClass' : 'removeClass';
+                $li.children('a')[method](c.anchorClass);
+            },
+            toggleTouchAction = function ($menu) {
+                var msTouchAction = $menu.css('ms-touch-action');
+                var touchAction = $menu.css('touch-action');
+                touchAction = touchAction || msTouchAction;
+                touchAction = (touchAction === 'pan-y') ? 'auto' : 'pan-y';
+                $menu.css({
+                    'ms-touch-action': touchAction,
+                    'touch-action': touchAction
+                });
+            },
+            getMenu = function ($el) {
+                return $el.closest('.' + c.menuClass);
+            },
+            getOptions = function ($el) {
+                return getMenu($el).data('sfOptions');
+            },
+            over = function () {
+                var $this = $(this),
+                    o = getOptions($this);
+                clearTimeout(o.sfTimer);
+                $this.siblings().superfish('hide').end().superfish('show');
+            },
+            close = function (o) {
+                o.retainPath = ($.inArray(this[0], o.$path) > -1);
+                this.superfish('hide');
+
+                if (!this.parents('.' + o.hoverClass).length) {
+                    o.onIdle.call(getMenu(this));
+                    if (o.$path.length) {
+                        $.proxy(over, o.$path)();
+                    }
+                }
+            },
+            out = function () {
+                var $this = $(this),
+                    o = getOptions($this);
+                if (ios) {
+                    $.proxy(close, $this, o)();
+                }
+                else {
+                    clearTimeout(o.sfTimer);
+                    o.sfTimer = setTimeout($.proxy(close, $this, o), o.delay);
+                }
+            },
+            touchHandler = function (e) {
+                var $this = $(this),
+                    o = getOptions($this),
+                    $ul = $this.siblings(e.data.popUpSelector);
+
+                if (o.onHandleTouch.call($ul) === false) {
+                    return this;
+                }
+
+                if ($ul.length > 0 && $ul.is(':hidden')) {
+                    $this.one('click.superfish', false);
+                    if (e.type === 'MSPointerDown' || e.type === 'pointerdown') {
+                        $this.trigger('focus');
+                    } else {
+                        $.proxy(over, $this.parent('li'))();
+                    }
+                }
+            },
+            applyHandlers = function ($menu, o) {
+                var targets = 'li:has(' + o.popUpSelector + ')';
+                if ($.fn.hoverIntent && !o.disableHI) {
+                    $menu.hoverIntent(over, out, targets);
+                }
+                else {
+                    $menu
+                        .on('mouseenter.superfish', targets, over)
+                        .on('mouseleave.superfish', targets, out);
+                }
+                var touchevent = 'MSPointerDown.superfish';
+                if (unprefixedPointerEvents) {
+                    touchevent = 'pointerdown.superfish';
+                }
+                if (!ios) {
+                    touchevent += ' touchend.superfish';
+                }
+                if (wp7) {
+                    touchevent += ' mousedown.superfish';
+                }
+                $menu
+                    .on('focusin.superfish', 'li', over)
+                    .on('focusout.superfish', 'li', out)
+                    .on(touchevent, 'a', o, touchHandler);
+            };
+
+        return {
+            // public methods
+            hide: function (instant) {
+                if (this.length) {
+                    var $this = this,
+                        o = getOptions($this);
+                    if (!o) {
+                        return this;
+                    }
+                    var not = (o.retainPath === true) ? o.$path : '',
+                        $ul = $this.find('li.' + o.hoverClass).add(this).not(not).removeClass(o.hoverClass).children(o.popUpSelector),
+                        speed = o.speedOut;
+
+                    if (instant) {
+                        $ul.show();
+                        speed = 0;
+                    }
+                    o.retainPath = false;
+
+                    if (o.onBeforeHide.call($ul) === false) {
+                        return this;
+                    }
+
+                    $ul.stop(true, true).animate(o.animationOut, speed, function () {
+                        var $this = $(this);
+                        o.onHide.call($this);
+                    });
+                }
+                return this;
+            },
+            show: function () {
+                var o = getOptions(this);
+                if (!o) {
+                    return this;
+                }
+                var $this = this.addClass(o.hoverClass),
+                    $ul = $this.children(o.popUpSelector);
+
+                if (o.onBeforeShow.call($ul) === false) {
+                    return this;
+                }
+
+                $ul.stop(true, true).animate(o.animation, o.speed, function () {
+                    o.onShow.call($ul);
+                });
+                return this;
+            },
+            destroy: function () {
+                return this.each(function () {
+                    var $this = $(this),
+                        o = $this.data('sfOptions'),
+                        $hasPopUp;
+                    if (!o) {
+                        return false;
+                    }
+                    $hasPopUp = $this.find(o.popUpSelector).parent('li');
+                    clearTimeout(o.sfTimer);
+                    toggleMenuClasses($this, o);
+                    toggleAnchorClass($hasPopUp);
+                    toggleTouchAction($this);
+                    // remove event handlers
+                    $this.off('.superfish').off('.hoverIntent');
+                    // clear animation's inline display style
+                    $hasPopUp.children(o.popUpSelector).attr('style', function (i, style) {
+                        if (typeof style !== 'undefined') {
+                            return style.replace(/display[^;]+;?/g, '');
+                        }
+                    });
+                    // reset 'current' path classes
+                    o.$path.removeClass(o.hoverClass + ' ' + c.bcClass).addClass(o.pathClass);
+                    $this.find('.' + o.hoverClass).removeClass(o.hoverClass);
+                    o.onDestroy.call($this);
+                    $this.removeData('sfOptions');
+                });
+            },
+            init: function (op) {
+                return this.each(function () {
+                    var $this = $(this);
+                    if ($this.data('sfOptions')) {
+                        return false;
+                    }
+                    var o = $.extend({}, $.fn.superfish.defaults, op),
+                        $hasPopUp = $this.find(o.popUpSelector).parent('li');
+                    o.$path = setPathToCurrent($this, o);
+
+                    $this.data('sfOptions', o);
+
+                    toggleMenuClasses($this, o, true);
+                    toggleAnchorClass($hasPopUp, true);
+                    toggleTouchAction($this);
+                    applyHandlers($this, o);
+
+                    $hasPopUp.not('.' + c.bcClass).superfish('hide', true);
+
+                    o.onInit.call(this);
+                });
+            }
+        };
+    })();
+
+    $.fn.superfish = function (method, args) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        }
+        else if (typeof method === 'object' || ! method) {
+            return methods.init.apply(this, arguments);
+        }
+        else {
+            return $.error('Method ' +  method + ' does not exist on jQuery.fn.superfish');
+        }
+    };
+
+    $.fn.superfish.defaults = {
+        popUpSelector: 'ul,.sf-mega', // within menu context
+        hoverClass: 'sfHover',
+        pathClass: 'overrideThisToUse',
+        pathLevels: 1,
+        delay: 800,
+        animation: {opacity: 'show'},
+        animationOut: {opacity: 'hide'},
+        speed: 'normal',
+        speedOut: 'fast',
+        cssArrows: true,
+        disableHI: false,
+        onInit: $.noop,
+        onBeforeShow: $.noop,
+        onShow: $.noop,
+        onBeforeHide: $.noop,
+        onHide: $.noop,
+        onIdle: $.noop,
+        onDestroy: $.noop,
+        onHandleTouch: $.noop
+    };
+
+})(jQuery, window);
+
+
+/*
+ * Supersubs v0.3b - jQuery plugin
+ * Copyright (c) 2013 Joel Birch
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ * 	http://www.opensource.org/licenses/mit-license.php
+ * 	http://www.gnu.org/licenses/gpl.html
+ *
+ *
+ * This plugin automatically adjusts submenu widths of suckerfish-style menus to that of
+ * their longest list item children. If you use this, please expect bugs and report them
+ * to the jQuery Google Group with the word 'Superfish' in the subject line.
+ *
+ */
+
+;(function($){ // $ will refer to jQuery within this closure
+
+    $.fn.supersubs = function(options){
+        var opts = $.extend({}, $.fn.supersubs.defaults, options);
+        // return original object to support chaining
+        return this.each(function() {
+            // cache selections
+            var $$ = $(this);
+            // support metadata
+            var o = $.meta ? $.extend({}, opts, $$.data()) : opts;
+            // cache all ul elements and show them in preparation for measurements
+            var $ULs = $$.find('ul').show();
+            // get the font size of menu.
+            // .css('fontSize') returns various results cross-browser, so measure an em dash instead
+            var fontsize = $('<li id="menu-fontsize">&#8212;</li>').css({
+                'padding' : 0,
+                'position' : 'absolute',
+                'top' : '-999em',
+                'width' : 'auto'
+            }).appendTo($$)[0].clientWidth; //clientWidth is faster than .width()
+            // remove em dash
+            $('#menu-fontsize').remove();
+            // loop through each ul in menu
+            $ULs.each(function(i) {
+                // cache this ul
+                var $ul = $(this);
+                // get all (li) children of this ul
+                var $LIs = $ul.children();
+                // get all anchor grand-children
+                var $As = $LIs.children('a');
+                // force content to one line and save current float property
+                var liFloat = $LIs.css('white-space','nowrap').css('float');
+                // remove width restrictions and floats so elements remain vertically stacked
+                $ul.add($LIs).add($As).css({
+                    'float' : 'none',
+                    'width'	: 'auto'
+                });
+                // this ul will now be shrink-wrapped to longest li due to position:absolute
+                // so save its width as ems.
+                var emWidth = $ul[0].clientWidth / fontsize;
+                // add more width to ensure lines don't turn over at certain sizes in various browsers
+                emWidth += o.extraWidth;
+                // restrict to at least minWidth and at most maxWidth
+                if (emWidth > o.maxWidth)		{ emWidth = o.maxWidth; }
+                else if (emWidth < o.minWidth)	{ emWidth = o.minWidth; }
+                emWidth += 'em';
+                // set ul to width in ems
+                $ul.css('width',emWidth);
+                // restore li floats to avoid IE bugs
+                // set li width to full width of this ul
+                // revert white-space to normal
+                $LIs.css({
+                    'float' : liFloat,
+                    'width' : '100%',
+                    'white-space' : 'normal'
+                })
+                    // update offset position of descendant ul to reflect new width of parent.
+                    // set it to 100% in case it isn't already set to this in the CSS
+                    .each(function(){
+                        var $childUl = $(this).children('ul');
+                        var offsetDirection = $childUl.css('left') !== undefined ? 'left' : 'right';
+                        $childUl.css(offsetDirection,'100%');
+                    });
+            }).hide();
+
+        });
+    };
+    // expose defaults
+    $.fn.supersubs.defaults = {
+        minWidth		: 9,		// requires em unit.
+        maxWidth		: 25,		// requires em unit.
+        extraWidth		: 0			// extra width can ensure lines don't sometimes turn over due to slight browser differences in how they round-off values
+    };
+
+})(jQuery); // plugin code ends
